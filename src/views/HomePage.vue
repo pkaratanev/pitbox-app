120a4f475ae6a65bbd1c667e07d79b7a668084db
pkaratanev <47246453+pkaratanev@users.noreply.github.com>
2023-07-19 21:10:52 +0300

    Finish infinite garage load

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -18,18 +18,23 @@ import {
   IonCardHeader,
   IonCardTitle,
   IonCardContent,
+  IonInfiniteScroll,
+  IonInfiniteScrollContent,
 } from "@ionic/vue";
 
 import { search, personCircle } from "ionicons/icons";
 
 const showSearch: Ref<boolean> = ref(false);
 const currentPage: Ref<number> = ref(1);
+const garages: Ref<any> = ref([]);
 
 const fetchGarages = () => {
   return axios
     .get(`/garages?page=${currentPage.value}`)
     .then((response: any) => {
-      console.log(response);
+      garages.value = [...garages.value, ...response.data];
+
+      currentPage.value++;
     })
     .catch((e: any) => {
       console.error(e);
@@ -37,11 +42,18 @@ const fetchGarages = () => {
 };
 
 const handleRefresh = (event: any) => {
-  setTimeout(() => {
-    fetchGarages().then(() => {
-      event.target.complete();
-    });
-  }, 1000);
+  currentPage.value = 1;
+  garages.value = [];
+
+  fetchGarages().then(() => {
+    event.target.complete();
+  });
+};
+
+const handleInfiniteScroll = (event: any) => {
+  fetchGarages().then(() => {
+    event.target.complete();
+  });
 };
 
 onMounted(() => {
@@ -74,13 +86,13 @@ onMounted(() => {
       <ion-refresher-content></ion-refresher-content>
     </ion-refresher>
 
-    <ion-card>
+    <ion-card v-for="garage in garages" :key="garage.id">
       <img
         alt="Silhouette of mountains"
         src="https://ionicframework.com/docs/img/demos/card-media.png"
       />
       <ion-card-header>
-        <ion-card-title>Card Title</ion-card-title>
+        <ion-card-title>{{ garage.name }}</ion-card-title>
       </ion-card-header>
 
       <ion-card-content>
@@ -89,8 +101,8 @@ onMounted(() => {
       </ion-card-content>
     </ion-card>
 
-    <!-- <ion-infinite-scroll @ionInfinite="ionInfinite">
+    <ion-infinite-scroll @ionInfinite="handleInfiniteScroll">
       <ion-infinite-scroll-content></ion-infinite-scroll-content>
-    </ion-infinite-scroll> -->
+    </ion-infinite-scroll>
   </ion-content>
 </template>
